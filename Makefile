5ee5a2483a1364b2505723715d6f27dda23ddbee
Ayusman Nanda <abhiseknanda799@gmail.com>
2026-06-09 22:07:06 +0530

    fix missing semicolon

---
+++ Makefile
@@ -0,0 +1,6 @@
+shell:
+	gcc -o main -c src/main.c
+
+clean:
+	rm -r src/main
+
--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv[]) {
 		for(int i = 0; i < argc; i++) {
 				printf("%s", argv[i]);
 		}
-		printf("\n")
+		printf("\n");
 		return 0;
 
 }
