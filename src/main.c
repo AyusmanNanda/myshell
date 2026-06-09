77d38e7acbd6f67f830bdd3026c5073c2716891d
Ayusman Nanda <abhiseknanda799@gmail.com>
2026-06-10 00:54:21 +0530

    add command overflow detection

--- src/main.c
+++ src/main.c
@@ -2,26 +2,46 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_LENGTH 100
+#define MAX_LENGTH 500
+
+int check_EOL(char buffer[]){
+		for(int i = 0; buffer[i] != '\0'; i++){
+				if(buffer[i] == '\n'){
+						return 1;
+				}
+		}
+		return 0;
+		
+
+}
+
 
 int main(int argc, char** argv[]) {
+		printf("\nWelcome to myshell! :)");
+		printf("\nPress ctrl + d to exit.");
 		for(int i = 1; i < argc; i++) {
 				printf("%s", argv[i]);
 		}
-		printf("\n");
-
 		char buffer[MAX_LENGTH];
 		char* status;
 start:
 		printf("\nmyshell>");
 		status = fgets(buffer, sizeof(buffer), stdin);
 		if(status != NULL){
+			if(!check_EOL(buffer)){
+				printf("\nError: command is too long !\nTry again !");
+				memset(buffer, '\0', sizeof(buffer));
+				scanf("%*[^\n]"); scanf("%*c");
+				goto start;
+			}
+			else{
 				printf("%s", buffer);
 				goto start;
+			}
 		}
 		else{
-				printf("Bye :)");
-				exit(1);
+				printf("\nBye :)");
+				exit(0);
 		}
 
 		return 0;
